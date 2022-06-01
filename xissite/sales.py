5c8b20a83b35902642d73b37071d52c114e774ae
Randybutternugs <adonisalexis@icloud.com>
2022-06-01 15:41:18 -0400

    Added Webhook, db and db viewer!

--- xissite/sales.py
+++ xissite/sales.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, Flask, redirect, render_template, request
+from flask import Blueprint, Flask, redirect, render_template, request, jsonify
 import os
 import stripe
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
+from .models import Customer
+from .constants import STRIPE_SECRET_KEY, STRIPE_PUBLISHABLE_KEY, HP_Price_id, endpoint_secret
 
 sales = Blueprint('sales', __name__)
 
-stripe.api_key = 'sk_test_51KyKprGjhGCkZIA1cVsqA0wQCz7gLnYSI9dgnkicu3ZiLlkR5fmsi9IbuObb30tIogXR1qSN3Gyae9ttlcmwc2VQ00oqXnbACF'
+stripe.api_key = STRIPE_SECRET_KEY
 
 YOUR_DOMAIN = 'http://localhost:5000'
 
@@ -15,7 +20,7 @@ def create_checkout_session():
             line_items=[
                 {
                     # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': 'price_1KyMGfGjhGCkZIA1EHcmGKe8',
+                    'price': HP_Price_id,
                     'quantity': 1,
                 },
             ],
@@ -36,3 +41,52 @@ def successfulpurchase():
 @sales.route('/cancel')
 def cancel():
     return render_template('cancel.html')
+
+#DATABASE VIEWER ------
+@sales.route('/viewdb', methods=['POST','GET'])
+def viewdatabase():
+    customerinf = Customer.query.order_by(Customer.id)
+    return render_template('show.html', customerinf=customerinf)
+
+
+#WEBHOOKS
+@sales.route('/webhook', methods=['POST', 'GET'])
+def webhook():
+    event = None
+    payload = request.data
+    sig_header = request.headers['STRIPE_SIGNATURE']
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        raise e
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        raise e
+
+    # Handle the event
+    #print('Unhandled event type {}'.format(event['type']))
+
+    if event['type'] == 'checkout.session.completed':
+        sesh = event['data']['object']
+        customer_email = sesh["customer_details"]["email"]
+        customer_name = sesh["customer_details"]["name"]
+        payment_intent = sesh["payment_intent"]
+        print(type(customer_email))
+        newly_customer = Customer(email = customer_email, name = customer_name)
+        db.session.add(newly_customer)
+        db.session.commit()
+        #print(session)
+        #print(customer_name)
+        #print(customer_email)
+        print("NEW CUSTOMER CREATED.")
+        #  - send an email to the customer
+    else:
+        print(event)
+        print('CUSTOMER ADD FAILED')
+
+
+    return jsonify(success=True)
