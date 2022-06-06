6b461fda12b9fae77f7e83c33f4cb8dbadc11044
Randybutternugs <adonisalexis@icloud.com>
2022-06-06 00:23:13 -0400

    Database security improvements

--- xissite/models.py
+++ xissite/models.py
@@ -1,24 +1,40 @@
+from flask_login import UserMixin
 from stripe import PaymentIntent
 from . import db
+from .constants import utherr, pahwur
 from sqlalchemy.sql import func
 import datetime
 
+current_product = "Hydroponics System V1"
+
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    name = db.Column(db.String(150))
-    date = db.Column(db.DateTime(timezone=True), default=func.current_timestamp()) #REMOVE LATER
-    #buys = db.relationship('purchase_info')
+    email = db.Column(db.String(150), unique=True, nullable = False)
+    name = db.Column(db.String(150), nullable = False)
+    creation_date = db.Column(db.DateTime(timezone=True), default=func.current_timestamp()) 
+    notes_on_customer = db.Column(db.String(500), nullable = True)
+    buys = db.relationship('Purchase_info', backref='customer')
 
 
-#    class purchase_info(db.Model):
- #       id = db.Column(db.Integer, primary_key=True)
-#
- #       customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
-#
- #       date = db.Column(db.DateTime(timezone=True), default=func.now())
-  #      prod_id = db.Column(db.String(200))
-   #     pay_Intent = db.Column(db.String(200))
-    #    shipping = db.Column(db.String(500))
-     #   billing = db.Column(db.String(500))
+class Purchase_info(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
     
+    product_name = db.Column(db.String(20), default = current_product)
+    purchase_date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    city = db.Column(db.String(500), nullable = False)
+    country = db.Column(db.String(500), nullable = False)
+    line1 = db.Column(db.String(500), nullable = False)
+    line2 = db.Column(db.String(500), nullable = True)
+    postal_code = db.Column(db.Integer, nullable = False)
+    state = db.Column(db.String(500), nullable = False)
+
+    pay_status = db.Column(db.String(50), default='Awaiting Payment')
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    user_name = db.Column(db.String(150), default = utherr)
+    pass_word = db.Column(db.String(150), default = pahwur)
--- xissite/sales.py
+++ xissite/sales.py
@@ -4,7 +4,7 @@ import stripe
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
-from .models import Customer
+from .models import Customer, Purchase_info
 from .constants import STRIPE_SECRET_KEY, STRIPE_PUBLISHABLE_KEY, HP_Price_id, endpoint_secret
 
 sales = Blueprint('sales', __name__)
@@ -17,6 +17,9 @@ YOUR_DOMAIN = 'http://localhost:5000'
 def create_checkout_session():
     try:
         checkout_session = stripe.checkout.Session.create(
+        shipping_address_collection={
+            'allowed_countries': ['US', 'CA'],
+        },
             line_items=[
                 {
                     # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -42,14 +45,8 @@ def successfulpurchase():
 def cancel():
     return render_template('cancel.html')
 
-#DATABASE VIEWER ------
-@sales.route('/viewdb', methods=['POST','GET'])
-def viewdatabase():
-    customerinf = Customer.query.order_by(Customer.id)
-    return render_template('show.html', customerinf=customerinf)
 
-
-#WEBHOOKS
+#WEBHOOKS ----------------
 @sales.route('/webhook', methods=['POST', 'GET'])
 def webhook():
     event = None
@@ -68,25 +65,89 @@ def webhook():
         raise e
 
     # Handle the event
-    #print('Unhandled event type {}'.format(event['type']))
-
+    # Handle the checkout.session.completed event
     if event['type'] == 'checkout.session.completed':
-        sesh = event['data']['object']
-        customer_email = sesh["customer_details"]["email"]
-        customer_name = sesh["customer_details"]["name"]
-        payment_intent = sesh["payment_intent"]
-        print(type(customer_email))
-        newly_customer = Customer(email = customer_email, name = customer_name)
-        db.session.add(newly_customer)
-        db.session.commit()
-        #print(session)
-        #print(customer_name)
-        #print(customer_email)
-        print("NEW CUSTOMER CREATED.")
-        #  - send an email to the customer
-    else:
-        print(event)
-        print('CUSTOMER ADD FAILED')
+        session = event['data']['object']
+
+        # Save an order in your database, marked as 'awaiting payment'
+        create_order(session)
+
+        # Check if the order is already paid (for example, from a card payment)
+        #
+        # A delayed notification payment will have an `unpaid` status, as
+        # you're still waiting for funds to be transferred from the customer's
+        # account.
+        if session.payment_status == "paid":
+        # Fulfill the purchase
+            print("Payment is Paid...n stuff.")
+            fulfill_order(session)
+
+    elif event['type'] == 'checkout.session.async_payment_succeeded':
+        session = event['data']['object']
+
+        # Fulfill the purchase
+    
+        fulfill_order(session)
+
+    elif event['type'] == 'checkout.session.async_payment_failed':
+        session = event['data']['object']
+
+        # Send an email to the customer asking them to retry their order
+        email_customer_about_failed_payment(session)
 
 
     return jsonify(success=True)
+
+def fulfill_order(session):
+  # TODO: fill me in
+  print("Fulfilling order")
+
+
+
+
+def create_order(session):
+    
+    customer_email = session["customer_details"]["email"]
+    customer_name = session["customer_details"]["name"]
+
+    #Make collecting shipping info easier
+    customer_ship_address = session["customer_details"]["address"]
+
+    #Breaking down shipping address dictionary object for easier appending to database... I know it looks rough but its quick, dirty and easier
+    c_city = customer_ship_address["city"]
+    c_country = customer_ship_address["country"]
+    c_line1 = customer_ship_address["line1"]
+    c_line2 = customer_ship_address["line2"]
+    c_postal = int(customer_ship_address["postal_code"])
+    c_state = customer_ship_address["state"]
+ 
+    paym_status = session["payment_status"]
+
+    #Check if customer already has made a purchase on site before
+    #If so, add purchase to existing customer entry
+    if Customer.query.filter(Customer.email == customer_email).first():
+        print("Pre-existing Customer/Buyer. Skipping Initial Customer Creation.\n")
+        
+        existing_customer = db.session.query(Customer).filter_by(email = customer_email).first()
+        newly_purchase = Purchase_info(city = c_city, country = c_country, line1 = c_line1, line2 = c_line2, postal_code = c_postal, state = c_state, pay_status = paym_status, customer = existing_customer)
+        db.session.add(newly_purchase)
+        db.session.commit()
+
+        print("New Purchase Added To Customer\n")
+
+    #If Customer has not made a purchase before (at least with email used to checkout) and is not in db, create new customer with purchase child 
+    else:
+        newly_customer = Purchase_info(city = c_city, country = c_country, line1 = c_line1, line2 = c_line2, postal_code = c_postal, state = c_state, pay_status = paym_status, customer = Customer(email = customer_email, name = customer_name))
+        db.session.add(newly_customer)
+        db.session.commit()
+        db.session.add(newly_customer)
+        db.session.commit()
+        print("Creating order for new customer")
+        
+        
+
+    
+
+def email_customer_about_failed_payment(session):
+  # TODO: fill me in
+    print("Emailing customer")
